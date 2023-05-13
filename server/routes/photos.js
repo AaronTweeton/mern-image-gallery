e08907d7b77ca795a58ee40cd34bb930ae344c4f
AaronTweeton <aaron@aarontweeton.com>
2023-05-13 07:14:35 -0700

    Added CRUD routes.

--- server/routes/photos.js
+++ server/routes/photos.js
@@ -1,9 +1,29 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET photos. */
+/* GET photo index. */
 router.get("/", function (req, res, next) {
-  res.send("Respond with photos");
+  res.json({ message: "Respond with photo index." });
+});
+
+/* Create photo */
+router.post("/", function (req, res, next) {
+  res.json({ message: "Created new photo." });
+});
+
+/* Read photo */
+router.get("/:id", function (req, res, next) {
+  res.json({ message: `Reading photo with ID: ${req.params.id}.` });
+});
+
+/* Update photo */
+router.patch("/:id", function (req, res, next) {
+  res.json({ message: `Updating photo with ID: ${req.params.id}.` });
+});
+
+/* Delete photo */
+router.delete("/:id", function (req, res, next) {
+  res.json({ message: `Deleting photo with ID: ${req.params.id}.` });
 });
 
 module.exports = router;
